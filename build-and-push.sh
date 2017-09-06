5cee87ae1f2ee5229e5862f7b2093db5e98283f7
Dao Hoang Son <daohoangson@gmail.com>
2017-09-06 10:19:56 +0700

    v1.37 -- version bump

--- build-and-push.sh
+++ build-and-push.sh
@@ -1,11 +1,22 @@
 #!/bin/sh
 
-LATEST_VERSION="1.36"
+set -e
+
+LATEST_VERSION="1.37"
 DOCKER_HUB_IMAGE="xfrocks/rclone"
 DOCKER_HUB_IMAGE_WITH_TAG="$DOCKER_HUB_IMAGE:$LATEST_VERSION"
 
 docker build --build-arg RCLONE_VERSION="v$LATEST_VERSION" -t "$DOCKER_HUB_IMAGE" .
 docker tag "$DOCKER_HUB_IMAGE" "$DOCKER_HUB_IMAGE_WITH_TAG"
 
+while true
+do
+  read -p "Push $DOCKER_HUB_IMAGE and $DOCKER_HUB_IMAGE_WITH_TAG? [yN]" yn
+  case $yn in
+    [Yy]* ) break;;
+    * )
+      exit 0;;
+  esac
+done
 docker push "$DOCKER_HUB_IMAGE"
 docker push "$DOCKER_HUB_IMAGE_WITH_TAG"
